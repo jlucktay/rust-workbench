6f34a1720a385b8b814511e3f78afc20e2ddcbb3
James Lucktaylor <jlucktay+github@gmail.com>
2022-07-16 15:26:54 +0100

    feat(grrs): add debug logger

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -4,23 +4,27 @@ use std::{
 };
 
 use clap::Parser;
+use log::debug;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
 	/// The pattern to look for
 	pattern: String,
+
 	/// The path to the file to read
 	#[clap(parse(from_os_str))]
 	path: std::path::PathBuf,
 }
 
 fn main() -> std::io::Result<()> {
+	env_logger::init();
+
 	let args = Cli::parse();
 
 	// Exercise for the reader: Make this program output its arguments!
-	println!("pattern: '{}'", args.pattern);
-	println!("path:    '{}'", args.path.display());
+	debug!("pattern: '{}'", args.pattern);
+	debug!("path:    '{}'", args.path.display());
 
 	/*
 	Exercise for the reader:
