b3dfe6660f14ebaf5c981fd7af2f991a9a869a02
junderw <junderwood@bitcoinbank.co.jp>
2022-08-17 00:25:00 +0900

    Implement WordCollection

--- pp2a-asg2/src/lib.rs
+++ pp2a-asg2/src/lib.rs
@@ -1,26 +1,54 @@
-pub trait WordCollection<'a> {
-	/// # Errors
-	///
-	/// The default implementation always fails.
-	fn make_collection(_words: &'a [&'a str]) -> Result<Self, WordCollectionCreateError>
-	where
-		Self: std::marker::Sized,
-	{
-		Err(WordCollectionCreateError::Failure)
+use std::fmt::Display;
+
+/// This is what holds the allocated data.
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct WordCollection {
+	words: Vec<String>,
+}
+
+impl WordCollection {
+	/// Create a new instance of the WordCollection
+	pub fn new() -> Self {
+		Self { words: Vec::new() }
+	}
+	/// Allocate the max size at once (24 bytes * capacity).
+	pub fn with_capacity(capacity: usize) -> Self {
+		Self {
+			words: Vec::with_capacity(capacity),
+		}
+	}
+
+	pub fn add_word(&mut self, c: &str) {
+		let i = self
+			.words
+			.iter()
+			.position(|s| s.as_str() > c)
+			.unwrap_or(self.words.len());
+		self.words.insert(i, c.to_string());
+	}
+
+	pub fn search(&self, c: &str) -> bool {
+		self.words.iter().any(|s| s.as_str() == c)
+	}
+
+	pub fn to_vec(&self) -> Vec<String> {
+		self.words.clone()
 	}
 }
 
-#[derive(Debug, Eq, PartialEq)]
-pub enum WordCollectionCreateError {
-	Failure,
+/// Implementing Display automatically gives you ToString (.to_string())
+/// and a bunch of other goodies.
+impl Display for WordCollection {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		for (i, word) in self.words.iter().enumerate() {
+			f.write_fmt(format_args!("Element #{}:\t{}\n", i + 1, word))?;
+		}
+		Ok(())
+	}
 }
 
-pub struct OrderedLinearArray<'a> {
-	words: &'a [&'a str],
-}
-
-impl<'a> WordCollection<'a> for OrderedLinearArray<'a> {
-	fn make_collection(words: &'a [&'a str]) -> Result<Self, WordCollectionCreateError> {
-		Ok(OrderedLinearArray { words })
+impl Default for WordCollection {
+	fn default() -> Self {
+		Self::new()
 	}
 }
--- pp2a-asg2/tests/unit.rs
+++ pp2a-asg2/tests/unit.rs
@@ -1,4 +1,4 @@
-use pp2a_asg2::{OrderedLinearArray, WordCollection};
+use pp2a_asg2::WordCollection;
 
 const NAMES: [&str; 8] = [
 	"Peter", "Sathish", "Wade", "Don", "Indrajit", "Rahul", "Sam", "Kevin",
@@ -7,8 +7,33 @@ const NAMES: [&str; 8] = [
 #[test]
 fn driver_ord_array_linear() {
 	assert_eq!(NAMES.len(), 8);
+	// sort using normal methods (for comparison)
+	let mut sorted: Vec<String> = NAMES.iter().map(|s| s.to_string()).collect();
+	sorted.sort();
 
-	let make_result = OrderedLinearArray::make_collection(NAMES.as_slice());
+	// create
+	let mut make_result = WordCollection::with_capacity(8);
+	// add all words (random order)
+	NAMES.iter().for_each(|s| make_result.add_word(s));
 
-	assert!(make_result.is_ok());
+	// Check sorted
+	assert_eq!(make_result.to_vec(), sorted);
+	// Check search
+	assert_eq!(make_result.search("Wade"), true);
+	assert_eq!(make_result.search("Sam"), true);
+	assert_eq!(make_result.search("Rahhh"), false);
+
+	// println!("{}", make_result); // or make_result.to_string()
+	/*
+		Element #1:     Don
+		Element #2:     Indrajit
+		Element #3:     Kevin
+		Element #4:     Peter
+		Element #5:     Rahul
+		Element #6:     Sam
+		Element #7:     Sathish
+		Element #8:     Wade
+	*/
+	// println!("{:?}", make_result);
+	/* WordCollection { words: ["Don", "Indrajit", "Kevin", "Peter", "Rahul", "Sam", "Sathish", "Wade"] } */
 }
