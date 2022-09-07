2f795bab980d570cdff764391d12bc153b20b20d
James Lucktaylor <jlucktay+github@gmail.com>
2022-09-07 12:24:11 +0100

    test(pp2a-asg2): optimise

--- pp2a-asg2/src/lib.rs
+++ pp2a-asg2/src/lib.rs
@@ -35,8 +35,16 @@ pub mod ubst;
 #[inline]
 #[must_use]
 pub fn fibonacci(n: u64) -> u64 {
-	match n {
-		0 | 1 => 1,
-		n => fibonacci(n - 1) + fibonacci(n - 2),
+	let mut a = 0;
+	let mut b = 1;
+
+	if n != 0 {
+		for _ in 0..n {
+			let c = a + b;
+			a = b;
+			b = c;
+		}
 	}
+
+	b
 }
