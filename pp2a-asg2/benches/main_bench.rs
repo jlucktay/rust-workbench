328e90b07046c42045cdcca868211936e967db2f
James Lucktaylor <jlucktay+github@gmail.com>
2022-09-07 12:29:17 +0100

    test(pp2a-asg2): build out some more benchmarks

--- pp2a-asg2/benches/main_bench.rs
+++ pp2a-asg2/benches/main_bench.rs
@@ -1,10 +1,29 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 
 use pp2a_asg2::fibonacci;
 
 fn criterion_benchmark(c: &mut Criterion) {
 	c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+	c.bench_function("fib 200", |b| b.iter(|| fibonacci(black_box(200))));
+	c.bench_function("fib 2_000", |b| b.iter(|| fibonacci(black_box(2_000))));
+	c.bench_function("fib 20_000", |b| b.iter(|| fibonacci(black_box(20_000))));
+	c.bench_function("fib 200_000", |b| b.iter(|| fibonacci(black_box(200_000))));
+	c.bench_function("fib 2_000_000", |b| {
+		b.iter(|| fibonacci(black_box(2_000_000)));
+	});
 }
 
-criterion_group!(benches, criterion_benchmark);
+const fn do_something(_size: usize) {
+	// Do something with the size
+}
+
+fn from_elem(c: &mut Criterion) {
+	let size: usize = 1024;
+
+	c.bench_with_input(BenchmarkId::new("input_example", size), &size, |b, &s| {
+		b.iter(|| do_something(s));
+	});
+}
+
+criterion_group!(benches, criterion_benchmark, from_elem);
 criterion_main!(benches);
