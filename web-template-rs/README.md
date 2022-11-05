e4974525522b9a8ae7f93f3ac4a7b812da5c978e
James Lucktaylor <jlucktay+github@gmail.com>
2022-11-05 12:35:57 +0000

    feat(web-template-rs): add code from article

---
+++ web-template-rs/README.md
@@ -0,0 +1,3 @@
+# A Rust web app with HTML templates
+
+From [here](https://woile.dev/posts/web-app-with-template-in-rust/).
--- web-template-rs/src/main.rs
+++ web-template-rs/src/main.rs
@@ -1,3 +1,76 @@
-fn main() {
-	println!("Hello, world!");
+use axum::{extract::Path, response::Html, routing::get, Router};
+use minijinja::render;
+use serde::Serialize;
+
+async fn home() -> Html<&'static str> {
+	Html("hello world")
 }
+
+async fn get_profile(Path(profile_name): Path<String>) -> Html<String> {
+	let orders_example = vec![
+		Items {
+			id: 1,
+			name: "Article banana".into(),
+		},
+		Items {
+			id: 2,
+			name: "Article apple".into(),
+		},
+	];
+	let profile_example = Profile {
+		full_name: profile_name,
+		items: orders_example,
+	};
+	let r = render!(PROFILE_TEMPLATE, profile => profile_example );
+	Html(r)
+}
+
+#[tokio::main]
+async fn main() {
+	let app = Router::new()
+		.route("/", get(home))
+		.route("/:profile_name", get(get_profile));
+	// run it with hyper on localhost:3000
+	axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+		.serve(app.into_make_service())
+		.await
+		.unwrap();
+}
+
+#[derive(Debug, Serialize)]
+struct Items {
+	id: i32,
+	name: String,
+}
+
+#[derive(Debug, Serialize)]
+struct Profile {
+	full_name: String,
+	items: Vec<Items>,
+}
+
+const PROFILE_TEMPLATE: &str = r#"
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>A Basic HTML5 Template</title>
+  <meta name="description" content="A basic HTML5 Template for new projects.">
+  <meta name="author" content="Woile">
+</head>
+
+<body>
+    <h1>Profile of {{ profile.full_name|title }}</h1>
+    <p>This is a template example to show some functionality</p>
+    <h2>Items</h3>
+    <ul>
+        {% for item in profile.items %}
+        <li>{{ item.name }} ({{ item.id }})</li>
+        {% endfor %}
+    <ul>
+</body>
+</html>
+"#;
