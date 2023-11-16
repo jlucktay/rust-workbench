452a00a14d29fc00df5e58bec452c4b5beec6571
James Lucktaylor <jlucktay+github@gmail.com>
2023-11-16 06:55:47 +0000

    feat(grrs): update dependencies and attributes

--- grrs/src/lib.rs
+++ grrs/src/lib.rs
@@ -21,11 +21,11 @@ pub fn _find_matches(
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser)]
+#[command(author, version, about, long_about = None)]
 pub struct Cli {
 	/// The pattern to look for
 	pub pattern: String,
 
 	/// The path to the file to read
-	#[clap(parse(from_os_str))]
 	pub path: std::path::PathBuf,
 }
